2521e4d3f595bcb6eeba895cb55ff6c0ffa2406c
am1553 <aryanmehta2016@gmail.com>
2021-11-18 17:39:17 +0000

    fixing bug

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // SELECTORS
 const themeBtn = document.getElementById("theme-btn");
 const todoInput = document.getElementById("todo-input-text");
+const tickButton = document.getElementsByClassName("tick");
+const crossButton = document.getElementsByClassName("cross");
+
 
 // EVENTLISTENERS
-themeBtn.addEventListener("click", themeToggle)
-todoInput.addEventListener("click", placeholderToggle)
+themeBtn.addEventListener("click", themeToggle);
+todoInput.addEventListener("click", placeholderToggle);
+Array.prototype.forEach.call(tickButton, child.addEventListener("click", completeTodo))
+
+
 // FUNCTIONS
 
 // toggle-dark-mode-light-mode
@@ -18,19 +24,24 @@ function themeToggle() {
     }
 }
 
-// add-remove-placeholder-onclick
-function placeholderToggle(event) {
+    // -------------add-remove-placeholder-onclick
+    function placeholderToggle(event) {
 
-    if(todoInput.placeholder === "Create a new todo...") {
-        todoInput.placeholder = "";
-    } else {
-        todoInput.placeholder = "Create a new todo...";
-    }
-    // if click outside the input then revert back placeholder
-    document.addEventListener("click", function(event) {
-        const innerClick = todoInput.contains(event.target);
-        if(!innerClick) {
+        if(todoInput.placeholder === "Create a new todo...") {
+            todoInput.placeholder = "";
+        } else {
             todoInput.placeholder = "Create a new todo...";
         }
-    })
+        // if click outside the input then revert back placeholder
+        document.addEventListener("click", function(event) {
+            const innerClick = todoInput.contains(event.target);
+            if(!innerClick) {
+                todoInput.placeholder = "Create a new todo...";
+            }
+        })
+    }
+
+// complete todo task
+function completeTodo() {
+
 }
