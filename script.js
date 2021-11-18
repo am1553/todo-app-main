aef5d9c87df555a381b4ea33514d17ba7bbf1ab6
am1553 <aryanmehta2016@gmail.com>
2021-11-18 19:51:08 +0000

    testing javascript functionality

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 // SELECTORS
 const themeBtn = document.getElementById("theme-btn");
 const todoInput = document.getElementById("todo-input-text");
-const tickButton = document.getElementsByClassName("tick");
-const crossButton = document.getElementsByClassName("cross");
+
 
 
 // EVENTLISTENERS
 themeBtn.addEventListener("click", themeToggle);
 todoInput.addEventListener("click", placeholderToggle);
-Array.prototype.forEach.call(tickButton, child.addEventListener("click", completeTodo))
-
 
 // FUNCTIONS
 
@@ -41,7 +38,61 @@ function themeToggle() {
         })
     }
 
-// complete todo task
-function completeTodo() {
+// TODO ADDING LIST AND REMOVING LISTS
+todoInput.addEventListener("keypress", function(e) {
+    const tickButton = document.getElementsByClassName("tick");
+    const crossButton = document.getElementsByClassName("cross");
 
-}
+    if(e.key === "Enter") {
+        const listContainer = document.getElementById("list-container");
+        let addList = todoInput.value;
+        
+        // create li
+        const li = document.createElement("li");
+        li.innerText = addList;
+
+        // create li buttons
+        const createCheckButton = document.createElement("button");
+        const createCrossButton = document.createElement("button");
+        createCheckButton.classList.add("tick");
+        createCrossButton.classList.add("cross");
+        createCheckButton.classList.add("circle");
+        createCrossButton.classList.add("circle");
+
+        // append child buttons to list
+        li.appendChild(createCheckButton);
+        li.appendChild(createCrossButton);
+
+
+        // append child to container
+        listContainer.appendChild(li);
+    }
+
+    for(let i = 0; i < tickButton.length; i++) {
+        tickButton[i].addEventListener("click", ()=> {
+            tickButton[i].classList.toggle("completed");
+            if(tickButton[i].classList.contains("completed")) {
+                tickButton[i].innerHTML = "<img src='images/icon-check.svg'>";
+                tickButton[i].parentElement.style.textDecoration = "line-through";
+
+            } else {
+                tickButton[i].innerHTML = "";
+                tickButton[i].parentElement.style.textDecoration = "";
+            }
+        })
+    }
+
+    // delete todo task
+    for(let i = 0; i < crossButton.length; i++) {
+        crossButton[i].addEventListener("click", ()=> {
+            crossButton[i].parentElement.style.display = "none";
+        })
+    }
+})
+
+
+
+    // complete todo task
+
+
+    
